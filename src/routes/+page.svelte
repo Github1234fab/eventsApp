e452d86a8a134ddcb2c2272f38e0d311f23e6b38
Github1234fab <fabien.marceau@gmail.com>
2024-12-18 22:26:01 +0100

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,44 @@
 <script>
-
         import UpLoadJson from "../components/UpLoadJson.svelte";
         import UpLoadJsonPub from "../components/UpLoadJsonPub.svelte";
         import Home from "../components/Home.svelte";
         import "../routes/styles.css";
+
+        let accessGranted = false;
+        let passwordInput = "";
+        const correctPassword = "Cal"; // Remplacez par votre mot de passe.
+
+        function checkPassword() {
+                if (passwordInput === correctPassword) {
+                        accessGranted = true;
+                } else {
+                        alert("Mot de passe incorrect !");
+                }
+        }
 </script>
 
 <main>
+        {#if accessGranted}
+    <!-- Votre application -->
+    
+
         <!-- <Calendar /> -->
-         <!-- <Filter /> -->
-         <Home />
+        <!-- <Filter /> -->
+        <Home />
         <UpLoadJson />
         <UpLoadJsonPub />
-
+        {:else}
+    <!-- Formulaire de protection -->
+    <div style="text-align: center; margin-top: 50px;">
+        <h1>Protection par mot de passe</h1>
+        <input
+            type="password"
+            placeholder="Entrez le mot de passe"
+            bind:value={passwordInput}
+        />
+        <button on:click={checkPassword}>Valider</button>
+    </div>
+{/if}
 </main>
 
 <style>
